b5828173edf697b61342c242ffb0db1ce33e1cce
Guest User <student@epicodus.com>
2015-04-22 15:33:51 -0700

    fully functional logic, sinatra and all tests passing

--- app.rb
+++ app.rb
@@ -2,3 +2,19 @@ require "sinatra"
 require "sinatra/reloader"
 require "./lib/PRS"
 also_reload"lib/**/*.rb"
+require 'sinatra/twitter-bootstrap'
+
+
+get('/') do
+
+  erb(:index)
+
+end
+
+get('/winning') do
+  @p1 = params.fetch('p1')
+  @p2 = params.fetch('p2')
+  @winning = @p1.beats(@p2)
+  erb(:winning)
+
+end
--- lib/PRS.rb
+++ lib/PRS.rb
@@ -1,21 +1,20 @@
 class String
   def beats(p2)
-    score = 0
+#    score = 0
     p1score = 0
     p2score = 0
 
     if self == "rock" && p2 == "scissors" || self == "paper" && p2 == "rock" || self == "scissors" && p2 == "paper"
 
-      p1score = p1score + 1
+       "Player one wins"
 
     elsif p2 == "rock" && self == "scissors" || p2 == "paper" && self == "rock" || p2 == "scissors" && self == "paper"
 
-      p2score = p2score + 1
+       "Player two wins"
 
     else self == p2
 
-      p1score = p1score + 0
-      p2score = p2score + 0
+       "Tie"
 
     end
   end
