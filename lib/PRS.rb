69f646f059f85a9026fe870613f5796088a286fa
Guest User <student@epicodus.com>
2015-04-22 13:59:30 -0700

    add login for self winning and p2 winning conditions with pasing tests

--- lib/PRS.rb
+++ lib/PRS.rb
@@ -1,11 +1,16 @@
 class String
   def beats(p2)
-
+    score = 0
     p1score = 0
     p2score = 0
 
     if self == "rock" && p2 == "scissors" || self == "paper" && p2 == "rock" || self == "scissors" && p2 == "paper"
-      score = p1score + 1
+
+      p1score = p1score + 1
+
+    else p2 == "rock" && self == "scissors" || p2 == "paper" && self == "rock" || p2 == "scissors" && self == "paper"
+
+      p2score = p2score + 1
     end
   end
 end
--- spec/PRS_spec.rb
+++ spec/PRS_spec.rb
@@ -7,7 +7,33 @@ Capybara.app = Sinatra::Application
 
 
 describe("String#beats") do
-  it ("returns true if rock is the object and scissors is the argument") do
+
+  it ("returns true if self selects rock and p2 selects scissors") do
     expect('rock'.beats('scissors')).to(eq(1))
   end
+
+  it("returns true if self selects paper and p2 selects rock") do
+    expect('paper'.beats('rock')).to(eq(1))
+  end
+
+  it("returns true if self selects scissors and p2 selects paper") do
+    expect('scissors'.beats('paper')).to(eq(1))
+  end
+
+  it("returns true if p2 selects rock and self selects scissors") do
+    expect('scissors'.beats('rock')).to(eq(1))
+  end
+
+  it("returns true if p2 selects paper and self selects rock") do
+    expect('rock'.beats('paper')).to(eq(1))
+  end
+
+  it("returns true if p2 selects scissors and self selects paper") do
+    expect('paper'.beats('scissors')).to(eq(1))
+  end
+
+  it("returns no score if self equals p2") do
+    expect('paper'.beats('paper')).to(eq(0))
+  end
+
 end
